154273c59d3439173c3309bcaff5a73dfc5fd868
Martim Leme <up202304944@up.pt>
2025-05-27 20:30:54 +0100

    Square appears on screen, and every second it appears elsewhere

--- Pingus/pingus.py
+++ Pingus/pingus.py
@@ -1,23 +1,49 @@
 import pygame as py 
 import random as rndm
+
+
 py.init()
 screen = py.display.set_mode((1280, 720))
 clock = py.time.Clock() 
+
+
+
+spawn_rating = 1000 # em milissegundos! 
+
+square = None
+spawn_time = py.time.get_ticks()
+
 running = True
 
 # função para desenhar os quadrados 
 def draw_square():
-    square = py.Rect(rndm.randint(50, 1230),rndm.randint(50, 680), 50, 50)
-    py.draw.rect(screen, (0,0,0), square)
+    return py.Rect(rndm.randint(50, 1230),rndm.randint(50, 680), 50, 50)
 
 while running:
+    currentTime = py.time.get_ticks()
+    
+    
     for event in py.event.get():
         if event.type == py.QUIT:
             running = False
     
+    
     screen.fill("white")
 
-    draw_square()
+
+    if square is None:
+        square = draw_square() 
+        spawn_time = currentTime
+    
+
+    elif currentTime - spawn_time >= spawn_rating: 
+        square = None
+
+    if square: 
+        py.draw.rect(screen, (0,0,0), square)
+    
+    
+       
     py.display.flip()
     clock.tick(60)
 
